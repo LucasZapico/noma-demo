f51c6833f28c55f6a3608cd26621659c63881e0d
Lucas zapico <zapicolucas@gmail.com>
2020-11-23 15:05:37 -0800

    consolidate helper into hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,60 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from "react"
+// import ResizeObserver from "resize-observer-polyfill"
+
+// export default function useMeasure() {
+//   const ref = useRef()
+//   const [bounds, set] = useState({ left: 0, top: 0, width: 0, height: 0 })
+//   const [ro] = useState(
+//     () => new ResizeObserver(([entry]) => set(entry.contentRect))
+//   )
+//   useEffect(() => {
+//     ro.observe(ref.current), ro.disconnect
+//     return () => {}
+//   }, [])
+//   return [{ ref }, bounds]
+// }
 
 // get window width on resize hook
 const getWidth = () => {
-  if (typeof window !== 'undefined') {
+  if (typeof window !== "undefined") {
     return (
       window.innerWidth ||
       document.documentElement.clientWidth ||
       document.body.clientWidth
-    );
+    )
   } else {
-    return 0;
+    return 0
   }
-};
+}
 
 export const useCurrentWidth = () => {
-  let [width, setWidth] = useState(getWidth());
+  let [width, setWidth] = useState(getWidth())
 
   useEffect(() => {
     const resizeListener = () => {
-      setWidth(getWidth());
-    };
-    window.addEventListener('resize', resizeListener);
+      setWidth(getWidth())
+    }
+    window.addEventListener("resize", resizeListener)
 
     return () => {
-      window.removeEventListener('resize', resizeListener);
-    };
-  }, []);
-  return width;
-};
-
+      window.removeEventListener("resize", resizeListener)
+    }
+  }, [])
+  return width
+}
 
 // get window width on resize hook
 const getHeight = () => {
-  if (typeof window !== 'undefined') {
+  if (typeof window !== "undefined") {
     return (
       window.innerHeight ||
       document.documentElement.clientHeight ||
       document.body.clientHeight
-    );
+    )
   } else {
-    return 0;
+    return 0
   }
-};
+}
 
 export const useCurrentHeight = () => {
-  let [height, setHeight] = useState(getHeight());
+  let [height, setHeight] = useState(getHeight())
 
   useEffect(() => {
     const resizeListener = () => {
-      setHeight(getHeight());
-    };
-    window.addEventListener('resize', resizeListener);
+      setHeight(getHeight())
+    }
+    window.addEventListener("resize", resizeListener)
 
     return () => {
-      window.removeEventListener('resize', resizeListener);
-    };
-  }, []);
-  return height;
-};
+      window.removeEventListener("resize", resizeListener)
+    }
+  }, [])
+  return height
+}
